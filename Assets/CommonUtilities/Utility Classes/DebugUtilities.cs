5bc9c8cf094cfff0cf9a8e44fd830dbc61a96b2f
gjh33 <manhappylife@yahoo.ca>
2018-04-09 20:27:09 -0400

    Some print options

---
+++ Assets/CommonUtilities/Utility Classes/DebugUtilities.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugUtilities {
+    public static void SperatorPrint(params object[] objs)
+    {
+        string[] strings = new string[objs.Length];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            strings[i] = objs[i].ToString();
+        }
+        Debug.Log(string.Join(" | ", strings));
+    }
+
+    public static void SeperatorPrint(string delim = " | ", params object[] objs)
+    {
+        string[] strings = new string[objs.Length];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            strings[i] = objs[i].ToString();
+        }
+        Debug.Log(string.Join(delim, strings));
+    }
+
+    public static void Print(params object[] objs)
+    {
+        string[] strings = new string[objs.Length];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            strings[i] = objs[i].ToString();
+        }
+        Debug.Log(string.Join(" ", strings));
+    }
+
+    public static void Checkpoint()
+    {
+        Debug.Log("Checkpoint");
+    }
+}
